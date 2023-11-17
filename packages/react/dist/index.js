d365a312aec3e2613b2f2a57540661e459b964fe
elton <eltonm.prado@gmail.com>
2023-11-17 12:34:21 -0300

    Design System in React - Configuring React package

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,22 +1,61 @@
-"use strict";
+'use strict'
+const __defProp = Object.defineProperty
+const __getOwnPropDesc = Object.getOwnPropertyDescriptor
+const __getOwnPropNames = Object.getOwnPropertyNames
+const __hasOwnProp = Object.prototype.hasOwnProperty
+const __export = (target, all) => {
+  for (const name in all)
+    __defProp(target, name, { get: all[name], enumerable: true })
+}
+const __copyProps = (to, from, except, desc) => {
+  if ((from && typeof from === 'object') || typeof from === 'function') {
+    for (const key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, {
+          get: () => from[key],
+          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
+        })
+  }
+  return to
+}
+const __toCommonJS = (mod) =>
+  __copyProps(__defProp({}, '__esModule', { value: true }), mod)
+
+// src/index.tsx
+const src_exports = {}
+__export(src_exports, {
+  App: () => App,
+})
+module.exports = __toCommonJS(src_exports)
 
 // ../tokens/dist/index.mjs
-var colors = {
-  white: "#FFF",
-  black: "#000",
-  gray100: "#E1E1E6",
-  gray200: "#A9A9B2",
-  gray400: "#7C7C8A",
-  gray500: "#505059",
-  gray600: "#323238",
-  gray700: "#29292E",
-  gray800: "#202024",
-  gray900: "#121214",
-  ignite300: "#00B37E",
-  ignite500: "#00875F",
-  ignite700: "#015F43",
-  ignite900: "#00291D"
-};
+const colors = {
+  white: '#FFF',
+  black: '#000',
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+  ignite300: '#00B37E',
+  ignite500: '#00875F',
+  ignite700: '#015F43',
+  ignite900: '#00291D',
+}
 
-// src/index.ts
-console.log(colors);
+// src/index.tsx
+const import_jsx_runtime = require('react/jsx-runtime')
+function App() {
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)('h1', {
+    style: { color: colors.ignite300 },
+    children: 'Hello World',
+  })
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 &&
+  (module.exports = {
+    App,
+  })
--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@ignite_ui/tokens'
-
-console.log(colors)
