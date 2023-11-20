f05a824973c39dc345ac895dcb3e390b795d3aac
elton <eltonm.prado@gmail.com>
2023-11-20 13:30:58 -0300

    Design System in React - Color Documentation

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -11,8 +11,8 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: [
-    "../src/**/*.stories.mdx", 
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/pages/**/*.stories.mdx", 
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
