b20b680237ae48ddf4340248d6a5f070412c6977
elton <eltonm.prado@gmail.com>
2023-11-20 11:16:51 -0300

    updating files

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -1,105 +1,99 @@
-// src/styles/index.ts
-import { createStitches, defaultThemeMap } from '@stitches/react'
-
-const __defProp = Object.defineProperty
-const __defProps = Object.defineProperties
-const __getOwnPropDescs = Object.getOwnPropertyDescriptors
-const __getOwnPropSymbols = Object.getOwnPropertySymbols
-const __hasOwnProp = Object.prototype.hasOwnProperty
-const __propIsEnum = Object.prototype.propertyIsEnumerable
-const __defNormalProp = (obj, key, value) =>
-  key in obj
-    ? __defProp(obj, key, {
-        enumerable: true,
-        configurable: true,
-        writable: true,
-        value,
-      })
-    : (obj[key] = value)
-const __spreadValues = (a, b) => {
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
   for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop)) __defNormalProp(a, prop, b[prop])
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
   if (__getOwnPropSymbols)
     for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop)) __defNormalProp(a, prop, b[prop])
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
     }
-  return a
-}
-const __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b))
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 
 // ../tokens/dist/index.mjs
-const colors = {
-  white: '#FFF',
-  black: '#000',
-  gray100: '#E1E1E6',
-  gray200: '#A9A9B2',
-  gray400: '#7C7C8A',
-  gray500: '#505059',
-  gray600: '#323238',
-  gray700: '#29292E',
-  gray800: '#202024',
-  gray900: '#121214',
-  ignite300: '#00B37E',
-  ignite500: '#00875F',
-  ignite700: '#015F43',
-  ignite900: '#00291D',
-}
-const space = {
-  1: '0.25rem',
-  2: '0.5rem',
-  3: '0.75rem',
-  4: '1rem',
-  5: '1.25rem',
-  6: '1.5rem',
-  7: '1.75rem',
-  8: '2rem',
-  10: '2.5rem',
-  12: '3rem',
-  16: '4rem',
-  20: '5rem',
-  40: '10rem',
-  64: '16rem',
-  80: '20rem',
-}
-const radii = {
-  px: '1px',
-  xs: '4px',
-  sm: '6px',
-  md: '8px',
-  lg: '16px',
-  full: '99999px',
-}
-const fonts = {
-  default: 'Roboto, sans-serif',
-  code: 'monospace',
-}
-const fontSizes = {
-  xxs: '0.625rem',
-  xs: '0.75rem',
-  sm: '0.875rem',
-  md: '1rem',
-  lg: '1.125rem',
-  xl: '1.25rem',
-  '2xl': '1.5rem',
-  '4xl': '2rem',
-  '5xl': '2.25rem',
-  '6xl': '3rem',
-  '7xl': '4rem',
-  '8xl': '4.5rem',
-  '9xl': '6rem',
-}
-const fontWeights = {
-  regular: '400',
-  medium: '500',
-  bold: '700',
-}
-const lineHeights = {
-  shorter: '125%',
-  short: '140%',
-  base: '160%',
-  tall: '180%',
-}
-const {
+var colors = {
+  white: "#FFF",
+  black: "#000",
+  gray100: "#E1E1E6",
+  gray200: "#A9A9B2",
+  gray400: "#7C7C8A",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292E",
+  gray800: "#202024",
+  gray900: "#121214",
+  ignite300: "#00B37E",
+  ignite500: "#00875F",
+  ignite700: "#015F43",
+  ignite900: "#00291D"
+};
+var space = {
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  10: "2.5rem",
+  12: "3rem",
+  16: "4rem",
+  20: "5rem",
+  40: "10rem",
+  64: "16rem",
+  80: "20rem"
+};
+var radii = {
+  px: "1px",
+  xs: "4px",
+  sm: "6px",
+  md: "8px",
+  lg: "16px",
+  full: "99999px"
+};
+var fonts = {
+  default: "Roboto, sans-serif",
+  code: "monospace"
+};
+var fontSizes = {
+  xxs: "0.625rem",
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.25rem",
+  "6xl": "3rem",
+  "7xl": "4rem",
+  "8xl": "4.5rem",
+  "9xl": "6rem"
+};
+var fontWeights = {
+  regular: "400",
+  medium: "500",
+  bold: "700"
+};
+var lineHeights = {
+  shorter: "125%",
+  short: "140%",
+  base: "160%",
+  tall: "180%"
+};
+
+// src/styles/index.ts
+import { createStitches, defaultThemeMap } from "@stitches/react";
+var {
   styled,
   css,
   globalCss,
@@ -107,11 +101,11 @@ const {
   getCssText,
   theme,
   createTheme,
-  config,
+  config
 } = createStitches({
   themeMap: __spreadProps(__spreadValues({}, defaultThemeMap), {
-    height: 'space',
-    width: 'space',
+    height: "space",
+    width: "space"
   }),
   theme: {
     colors,
@@ -120,32 +114,34 @@ const {
     fonts,
     lineHeights,
     radii,
-    space,
-  },
-})
+    space
+  }
+});
 
 // src/index.tsx
-const Button = styled('button', {
-  fontFamily: '$default',
-  background: '$ignite300',
-  borderRadius: '$md',
+var Button = styled("button", {
+  fontFamily: "$default",
+  background: "$ignite300",
+  borderRadius: "$md",
   border: 0,
-  fontWeight: 'bold',
-  color: '$white',
+  fontWeight: "bold",
+  color: "$white",
   variants: {
     size: {
       small: {
         fontSize: 14,
-        padding: '$2 $4',
+        padding: "$2 $4"
       },
       big: {
         fontSize: 16,
-        padding: '$3 $6',
-      },
-    },
+        padding: "$3 $6"
+      }
+    }
   },
   defaultVariants: {
-    size: 'small',
-  },
-})
-export { Button }
+    size: "small"
+  }
+});
+export {
+  Button
+};
